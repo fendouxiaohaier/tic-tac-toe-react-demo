8778dcd5c090334f7e2b394cd97a089c20eedc41
liyang <cdjv_liyang@163.com>
2023-09-25 19:30:53 +0800

    chore: 创建typings文件夹，增加描述文件

--- src/components/TSDemo/index.tsx
+++ src/components/TSDemo/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, memo, useEffect } from "react";
 
 // import logo from '../../../public/logo192.png';
 
-import { Square } from "../../components/index.js";
+import { Square } from "../../components";
 
 const TSDemo: FC<{ demoValue: string }> = ({ demoValue }) => {
 
--- typings/components/index.d.ts
+++ typings/components/index.d.ts
@@ -2,7 +2,7 @@ import { Board } from "../components/Board/index";
 import { Square } from "../components/Square/index";
 import { Game } from "../components/Game/index";
 
-declare module "*.js" {
+declare module "components" {
   export { Board, Square, Game };
 }
 
