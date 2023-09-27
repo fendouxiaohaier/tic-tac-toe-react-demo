f25c402797f02b62be3a878909b440e21c3620c1
liyang <cdjv_liyang@163.com>
2023-09-27 11:49:53 +0800

    fix: 配置别名

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { Game } from "./components/index.js";
+import { Game } from "aaa/components";
 
 import "./index.css";
 
---
+++ typings/aaa/components/index.d.ts
@@ -0,0 +1,3 @@
+export { Board } from "./Board/index";
+export { Square } from "./Square/index";
+export { Game } from "./Game/index";
