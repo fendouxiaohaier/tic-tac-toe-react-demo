daf27f1b8c89486cbd4493875c6d4b6d88698a23
liyang <cdjv_liyang@163.com>
2023-09-28 13:08:29 +0800

    feat: tsconfig配置rootDirs

--- src/components/TSDemo/index.tsx
+++ src/components/TSDemo/index.tsx
@@ -4,19 +4,17 @@ import React, { FC, memo, useEffect } from "react";
 
 // import { Square } from "../../components/index.js";
 
-import foo from "foo";
 
 import { Square } from "../../components/index";
 
 const TSDemo: FC<{ demoValue: string }> = ({ demoValue }) => {
   useEffect(() => {
-    console.log(foo);
   }, []);
 
   return (
     <div style={{ margin: "100px" }}>
       <button>{demoValue}</button>
-      <Square children={<span>123</span>} coordinate={[1, 2]} index={1} />
+      <Square children={<span>123</span>} coordinate={[1, 2]} index={1} isLightBtn value="X"/>
     </div>
   );
 };
--- types/components/index.d.ts
+++ types/components/index.d.ts
@@ -1,8 +1,3 @@
 export { default as Board } from "./Board/index";
 export { default as Square } from "./Square/index";
 export { default as Game } from "./Game/index";
-
-declare module "./components" {
-  import { default as Game } from "./Game/index";
-  export { Game };
-}
